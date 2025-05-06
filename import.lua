3a6092016d7ab7cf7f97d275ee7039bfab06a239
IoriBranford <ioribranford@gmail.com>
2025-05-05 19:00:28 -0700

    Refactor in prep for importing from pack

--- import.lua
+++ import.lua
@@ -40,46 +40,46 @@ local StandardAnimations = {
     "idle", "run", "climb", "jump", "sit", "emote",
     "combat_idle", "backslash", "halfslash",
 
-    spellcast = { file = "standard/spellcast.png", s = 64, x = 0, y = 0, w = 448, h = 256 },
-    thrust = { file = "standard/thrust.png", s = 64, x = 0, y = 256, w = 512, h = 256,
+    spellcast = { file = "standard/spellcast.png", s = 64 },
+    thrust = { file = "standard/thrust.png", s = 64,
         parts = {
             "Windup", "Attack",
             Windup = { 0, 3 },
             Attack = { 4, 7 }
         },
-        [192] = { file = "custom/thrust_oversize.png" }
+        [192] = { file = "custom/thrust_oversize.png", s = 192 }
     },
     stand = {
-        file = "standard/walk.png", s = 64, x = 0, y = 512, w = 64, h = 256,
+        file = "standard/walk.png", s = 64, x = 0, y = 0, w = 64, h = 256,
         [128] = { file = "custom/walk_128.png", s = 128, x = 0, y = 0, w = 128, h = 512 },
     },
     walk = {
-        file = "standard/walk.png", s = 64, x = 64, y = 512, w = 512, h = 256,
+        file = "standard/walk.png", s = 64, x = 64, y = 0, w = 512, h = 256,
         [128] = { file = "custom/walk_128.png", s = 128, x = 128, y = 0, w = 1024, h = 512 },
     },
-    slash = { file = "standard/slash.png", s = 64, x = 0, y = 768, w = 384, h = 256,
+    slash = { file = "standard/slash.png", s = 64,
         parts = {
             "Windup", "Attack",
             Windup = { 0, 2 }, Attack = { 3, 5 }
         }
     },
-    shoot = { file = "standard/shoot.png", s = 64, x = 0, y = 1024, w = 832, h = 256,
+    shoot = { file = "standard/shoot.png", s = 64,
         parts = {
             "Windup", "Attack",
             Windup = { 0, 8 }, Attack = { 9, 11 }
         }
     },
-    hurt = { file = "standard/hurt.png", s = 64, x = 0, y = 1280, w = 384, h = 64,
+    hurt = { file = "standard/hurt.png", s = 64,
         parts = {
             "Knees", "Flat", "RiseToKnees", "RiseToFeet",
             Knees = { 0, 2 }, Flat = { 3, 5 }, RiseToFeet = { 5, 0 }, RiseToKnees = { 5, 3 }
         }
     },
 
-    climb = { file = "standard/climb.png", s = 64, x = 0, y = 1344, w = 384, h = 64},
-    idle = { file = "standard/idle.png", s = 64, x = 0, y = 1408, w = 128, h = 256 },
-    jump = { file = "standard/jump.png", s = 64, x = 0, y = 1664, w = 320, h = 256},
-    sit = { file = "standard/sit.png", s = 64, x = 0, y = 1920, w = 192, h = 256,
+    climb = { file = "standard/climb.png", s = 64 },
+    idle = { file = "standard/idle.png", s = 64 },
+    jump = { file = "standard/jump.png", s = 64 },
+    sit = { file = "standard/sit.png", s = 64,
         parts = {
             "GroundMasc", "GroundFem", "Chair",
             GroundMasc = {0, 0},
@@ -87,7 +87,7 @@ local StandardAnimations = {
             Chair = {2, 2}
         }
     },
-    emote = { file = "standard/emote.png", s = 64, x = 0, y = 2176, w = 192, h = 256,
+    emote = { file = "standard/emote.png", s = 64,
         parts = {
             "HandsOnHips", "HandsBehindBack", "Surprised",
             HandsOnHips = {0, 0},
@@ -95,9 +95,9 @@ local StandardAnimations = {
             Surprised = {2, 2}
         }
     },
-    run = { file = "standard/run.png", s = 64, x = 0, y = 2432, w = 512, h = 256 },
-    combat_idle = { file = "standard/combat_idle.png", s = 64, x = 0, y = 2688, w = 128, h = 256 },
-    backslash = { file = "standard/backslash.png", s = 64, x = 0, y = 2944, w = 832, h = 256,
+    run = { file = "standard/run.png", s = 64 },
+    combat_idle = { file = "standard/combat_idle.png", s = 64 },
+    backslash = { file = "standard/backslash.png", s = 64,
         parts = {
             "Windup", "Attack", "BackWindup", "BackAttack",
             Windup = { 0, 0 },
@@ -105,9 +105,9 @@ local StandardAnimations = {
             BackWindup = { 6, 7 },
             BackAttack = { 8, 12 }
         },
-        [192] = { file = "custom/slash_reverse_oversize.png" }
+        [192] = { file = "custom/slash_reverse_oversize.png", s = 192 }
     },
-    halfslash = { file = "standard/halfslash.png", s = 64, x = 0, y = 3200, w = 384, h = 256,
+    halfslash = { file = "standard/halfslash.png", s = 64,
         parts = {
             "Windup", "Attack",
             Windup = { 0, 0 },
@@ -118,36 +118,23 @@ local StandardAnimations = {
 
 local StandardSheetHeight = 3456
 
-local function animationRowFramesInSheet(animation, row, sheet)
-    local sourceFrameSize = animation.s
-
-    local sourceY = animation.y + row*sourceFrameSize
-    local maxY = sheet.height - sourceFrameSize
-    if sourceY > maxY then
-        return 0
-    end
-
-    local width = math.min(animation.w, sheet.width - animation.x)
-    return math.floor(width / sourceFrameSize)
-end
-
 ---@param sprite Sprite
 ---@param sheet Image
+---@param rowrect Rectangle
 ---@param animation LPCAnimation
----@param name string
----@param row integer
----@param dir integer|string
+---@param animname string
+---@param rowname integer|string
 ---@param args ImportLPCCharacterArgs
-local function importAnimation(sprite, sheet, animation, name, row, dir, args)
+local function importAnimationRow(sprite, sheet, rowrect, animation, animname, rowname, args)
     local frameDuration = args.frametime
     local outputFrameSize = sprite.width
     local layer = sprite.layers[1]
     local frames = sprite.frames
-    local sourceFrameSize = animation.s
-    local columns = animation.columns
+    local sourceFrameSize = rowrect.h or rowrect.height
+    local columns = math.floor((rowrect.w or rowrect.width) / sourceFrameSize)
     local maxX = sheet.width - sourceFrameSize
     local maxY = sheet.height - sourceFrameSize
-    local sourceRect = Rectangle(animation.x, animation.y + row*sourceFrameSize, sourceFrameSize, sourceFrameSize)
+    local sourceRect = Rectangle(rowrect.x, rowrect.y, sourceFrameSize, sourceFrameSize)
     local dest = Point(outputFrameSize/2 - sourceFrameSize/2, outputFrameSize/2 - sourceFrameSize/2)
     local fromFrameNumber = #frames
     local toFrameNumber = #frames + columns - 1
@@ -163,13 +150,13 @@ local function importAnimation(sprite, sheet, animation, name, row, dir, args)
     end
 
     local tag = sprite:newTag(fromFrameNumber, toFrameNumber)
-    tag.name = name..dir
+    tag.name = animname..rowname
 
     local parts = animation.parts
     if parts then
         local enabledAnimations = args.animationsExportEnabled
         for _, part in ipairs(parts) do
-            if not enabledAnimations or enabledAnimations[name..part] then
+            if not enabledAnimations or enabledAnimations[animname..part] then
                 local range = parts[part]
                 local from, to = range[1], range[2]
                 local direction
@@ -179,7 +166,7 @@ local function importAnimation(sprite, sheet, animation, name, row, dir, args)
                 end
                 tag = sprite:newTag(fromFrameNumber + range[1], fromFrameNumber + range[2])
                 tag.aniDir = direction
-                tag.name = name..part..dir
+                tag.name = animname..part..rowname
             end
         end
     end
@@ -201,42 +188,55 @@ local function importExtraAnimations(sprite, sheet, args)
     local extraAnimation = {
         file = "",
         s = outputFrameSize,
-        x = 0,
-        y = StandardSheetHeight,
-        w = extracolumns*outputFrameSize,
-        h = extrarows*outputFrameSize,
-        rows = extrarows,
-        columns = extracolumns
+    }
+    local dirrect = {
+        x = 0, y = StandardSheetHeight, w = extracolumns*outputFrameSize, h = outputFrameSize
     }
     for i = 0, extrarows-1 do
-        importAnimation(sprite, sheet, extraAnimation, "extra", i, i, args)
+        importAnimationRow(sprite, sheet, dirrect, extraAnimation, "extra", i, args)
+        dirrect.y = dirrect.y + outputFrameSize
     end
 end
 
----comment
+local function importAnimation(sprite, sheet, animrect, animation, basename, args)
+    local animh = animation.h or sheet.height
+    local rowrect = {
+        x = animation.x or 0,
+        y = animation.y or 0,
+        w = animation.w or sheet.width,
+        h = animation.s
+    }
+    if animrect then
+        rowrect.x = animrect.x or rowrect.x
+        rowrect.y = animrect.y or rowrect.y
+        rowrect.w = animrect.w or rowrect.w
+        animh = animrect.h or animh
+    end
+
+    local rows = math.floor(animh / animation.s)
+    if rows == 1 then
+        importAnimationRow(sprite, sheet, rowrect, animation, basename, "", args)
+    elseif rows > 1 then
+        rowrect.y = rowrect.y + rows*animation.s
+        for r = rows, 1, -1 do
+            rowrect.y = rowrect.y - animation.s
+            importAnimationRow(sprite, sheet, rowrect, animation, basename, rows-r, args)
+        end
+    end
+end
+
 ---@param sprite Sprite
 ---@param sheet Image
 ---@param animationSet AnimationSet
 ---@param args ImportLPCCharacterArgs
-local function importAnimationSet(sprite, sheet, animationSet, args)
+local function importAnimationSet(sprite, sheet, animationSet, animationrects, args)
     local enabledAnimations = args.animationsExportEnabled
 
     for _, basename in ipairs(animationSet) do
         if enabledAnimations[basename] then
             local animation = animationSet[basename]
-            animation.columns = math.floor(animation.w / animation.s)
-            animation.rows = math.floor(animation.h / animation.s)
-
-            if animationRowFramesInSheet(animation, 0, sheet) > 0 then
-                local rows = animation.rows
-                if rows <= 1 then
-                    importAnimation(sprite, sheet, animation, basename, 0, "", args)
-                else
-                    for r = rows, 1, -1 do
-                        importAnimation(sprite, sheet, animation, basename, r-1, rows-r, args)
-                    end
-                end
-            end
+            local srcrect = animationrects and animationrects[basename]
+            importAnimation(sprite, sheet, srcrect, animation, basename, args)
         end
     end
 end
@@ -245,10 +245,6 @@ local import = {
     StandardAnimations = StandardAnimations
 }
 
-function import.sheetHasAnimationRow(animation, row, sheet)
-    return animationRowFramesInSheet(animation, row, sheet) > 0
-end
-
 ---@param sheetsprite Sprite
 ---@param args ImportLPCCharacterArgs
 function import.FromSheet(sheetsprite, args)
@@ -256,7 +252,7 @@ function import.FromSheet(sheetsprite, args)
     sprite.filename = args.outputFile
     app.transaction("Import LPC Character Sheet", function()
         local sheet = Image(sheetsprite)
-        importAnimationSet(sprite, sheet, StandardAnimations, args)
+        importAnimationSet(sprite, sheet, StandardAnimations, StandardAnimationSheetRects, args)
         importExtraAnimations(sprite, sheet, args)
     end)
     return sprite
