3ee083328cd9e4767ad7761d3a9928e32ea434e2
IoriBranford <ioribranford@gmail.com>
2026-04-01 18:32:04 -0700

    Include shell script in zip

--- zip.sh
+++ zip.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip import-lpc-character.aseprite-extension package.json README.md lpc/*.lua *.lua -x __pref.lua
+zip import-lpc-character.aseprite-extension package.json README.md lpc/*.lua *.lua lpc2ase -x __pref.lua
